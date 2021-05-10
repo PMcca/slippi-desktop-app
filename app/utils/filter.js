8f254fb8eaf0a786f615cc054868c3730c158fb8
PMcca <petemcca@gmail.com>
2021-05-10 22:32:30 +0100

    Generates character filter list via function. Starts stage filter.

--- app/utils/filter.js
+++ app/utils/filter.js
@@ -1,60 +1,42 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
-// import { stages as stageUtils } from '@slippi/slippi-js';
+import { stages as stageUtils, characters as charUtils } from '@slippi/slippi-js';
 import types from '@slippi/slippi-js'
-import { characters as charUtils } from '@slippi/slippi-js'
+// import { characters as charUtils } from '@slippi/slippi-js'
 import getLocalImage from './image';
 import styles from '../components/FileLoader.scss';
 
-// Values are based on mappings between character and Slippi's numerical index
-// but in the filter they are represented alphabetically.
-const characterList = [
-  { value: "5", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-5-0.png")} inline={true} spaced='bottom'/>Bowser</div> },
-  { value: "0", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-0-0.png")} inline={true} spaced='bottom'/>Captain Falcon</div> },
-  { value: "1", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-1-0.png")} inline={true} spaced='bottom'/>Donkey Kong</div> },
-  { value: "22", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-22-0.png")} inline={true} spaced='bottom'/>Dr. Mario</div> },
-  { value: "20", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-20-0.png")} inline={true} spaced='bottom'/>Falco</div> },
-  { value: "2", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-2-0.png")} inline={true} spaced='bottom'/>Fox</div> },
-  { value: "25", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-25-0.png")} inline={true} spaced='bottom'/>Ganondorf </div> },
-  { value: "14", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-14-0.png")} inline={true} spaced='bottom'/>Ice Climbers</div> },
-  { value: "15", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-15-0.png")} inline={true} spaced='bottom'/>Jigglypuff </div> },
-  { value: "4", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-4-0.png")} inline={true} spaced='bottom'/>Kirby </div> },
-  { value: "6", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-6-0.png")} inline={true} spaced='bottom'/>Link </div> },
-  { value: "7", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-7-0.png")} inline={true} spaced='bottom'/>Luigi </div> },
-  { value: "8", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-8-0.png")} inline={true} spaced='bottom'/>Mario </div> },
-  { value: "9", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-9-0.png")} inline={true} spaced='bottom'/>Marth </div> },
-  { value: "10", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-10-0.png")} inline={true} spaced='bottom'/>Mewtwo </div> },
-  { value: "3", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-3-0.png")} inline={true} spaced='bottom'/>Mr. Game & Watch</div> },
-  { value: "11", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-11-0.png")} inline={true} spaced='bottom'/>Ness </div> },
-  { value: "24", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-24-0.png")} inline={true} spaced='bottom'/>Pichu </div> },
-  { value: "13", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-13-0.png")} inline={true} spaced='bottom'/>Pikachu </div> },
-  { value: "12", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-12-0.png")} inline={true} spaced='bottom'/>Peach </div> },
-  { value: "18", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-18-0.png")} inline={true} spaced='bottom'/>Zelda </div> },
-  { value: "23", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-23-0.png")} inline={true} spaced='bottom'/>Roy </div> },
-  { value: "16", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-16-0.png")} inline={true} spaced='bottom'/>Samus </div> },
-  { value: "19", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-19-0.png")} inline={true} spaced='bottom'/>Sheik </div> },
-  { value: "17", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-17-0.png")} inline={true} spaced='bottom'/>Yoshi </div> },
-  { value: "21", label: <div><Image className={styles["filter-list"]} src={getLocalImage("stock-icon-21-0.png")} inline={true} spaced='bottom'/>Young Link</div> },
-]
-
-const tst = generateCharacterList()
+const characterList = generateCharacterList();
 
 export function getCharacterList() {
   return characterList;
 }
 
 function generateCharacterList() {
-  let chars = [];
+  const chars = [];
   let i;
   for(i = 0; i < 26; i++) {
     const charName = charUtils.getCharacterName(i)
-    chars.push({ value: i, label: <div><Image className={styles["filter-list"]} src={getLocalImage(`stock-icon-${  i  }-0.png`)} inline={true} spaced='bottom'/>${ charName }</div> })
+    chars.push({ value: i, label: <div><Image className={styles["filter-list"]} src={getLocalImage(`stock-icon-${  i  }-0.png`)} inline={true} spaced='bottom'/>{ charName }</div> , name:charName})
   }
-  return chars;
+  // Return list sorted alphabetically
+  return chars.sort((a, b) =>  a.name.localeCompare(b.name));
+}
+
+function generateStageList() {
+  let stgs = types.Stage
+  const stages = [];
+  let i;
+  for(i = 2; i < 29; i++) {
+    const stage = stageUtils.getStageName(i);
+    stages.push(stage);
+  }
+  return stages;
 }
 
 // Filter a list of files given a list of characters to filter by
 export function filterCharacters(files, chars) {
+  generateStageList();
   let filteredFiles = files
   chars.forEach(char => {
     filteredFiles = filteredFiles.filter(file => {
